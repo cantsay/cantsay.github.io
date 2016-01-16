eb11c605007955d36ce464442e0aa0cdc9664ef8
cantsay <jake.c.king@live.com.au>
2016-01-16 19:19:34 +1100

    added dex sets up to ferro and including serp

--- script_res/setdex_nuggetBridge.js
+++ script_res/setdex_nuggetBridge.js
@@ -1,4 +1,289 @@
 ﻿var SETDEX_NUGGETBRIDGE={
+  "Aegislash": {
+    "THE CRUMBLER (Special Attacker)": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "sa": "252",
+        "sd": "4"
+      },
+      "ivs": {
+        "sp": "0"
+      },
+      "nature": "Quiet",
+      "ability": "Stance Change",
+      "item": "Weakness Policy",
+      "moves": [
+        "Shadow Ball",
+        "Shadow Sneak",
+        "Sacred Sword",
+        "Flash Cannon"
+      ]
+    },
+    "Substitute": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "nature": "Timid",
+      "ability": "Stance Change",
+      "item": "Leftovers",
+      "moves": [
+        "Substitute",
+        "Toxic",
+        "Shadow Ball",
+        "Flash Cannon"
+      ]
+    },
+    "Physical Attacker": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "at": "252",
+        "sd": "4"
+      },
+      "ivs": {
+        "sp": "0"
+      "nature": "Brave",
+      "ability": "Stance Change",
+      "item": "Life Orb",
+      "moves": [
+        "Shadow Sneak",
+        "Sacred Sword",
+        "Iron Head",
+        "Shadow Claw"
+      ]
+    },
+    "DragonWhale's SubToxic": {
+      "level": 50,
+      "evs": {
+        "hp": "224",
+        "sa": "156",
+        "sd": "36",
+        "sp": "92"
+      },
+      "nature": "Modest",
+      "ability": "Stance Change",
+      "item": "Leftovers",
+      "moves": [
+        "Substitute",
+        "Toxic",
+        "Shadow Ball",
+        "Flash Cannon"
+      ]
+    }
+  },
+  "Azumarill": {
+    "Belly Drum": {
+      "level": 50,
+      "evs": {
+        "hp": "228",
+        "at": "252",
+        "df": "12",
+        "sd": "12",
+        "sp": "4"
+      },
+      "nature": "Adamant",
+      "ability": "Huge Power",
+      "item": "Sitrus Berry",
+      "moves": [
+        "Aqua Jet",
+        "Play Rough",
+        "Waterfall",
+        "Superpower"
+      ]
+    },
+    "All Out Attcker": {
+      "level": 50,
+      "evs": {
+        "hp": "228",
+        "at": "252",
+        "df": "12",
+        "sd": "12",
+        "sp": "4"
+      },
+      "nature": "Adamant",
+      "ability": "Huge Power",
+      "item": "Assault Vest",
+      "moves": [
+        "Aqua Jet",
+        "Play Rough",
+        "Waterfall",
+        "Knock Off"
+      ]
+    },
+    "Choice Band": {
+      "level": 50,
+      "evs": {
+        "hp": "228",
+        "at": "252",
+        "df": "12",
+        "sd": "12",
+        "sp": "4"
+      },
+      "nature": "Adamant",
+      "ability": "Huge Power",
+      "item": "Choice Band",
+      "moves": [
+        "Aqua Jet",
+        "Play Rough",
+        "Waterfall",
+        "Superpower"
+      ]
+    }
+  },
+  "Beedrill": {
+    "Offensive Pivot": {
+      "level": 50,
+      "evs": {
+        "at": "228",
+        "df": "36",
+        "sp": "244"
+      },
+      "nature": "Jolly",
+      "ability": "Swarm",
+      "item": "Beedrillite",
+      "moves": [
+        "U-turn",
+        "Poison Jab",
+        "Drill Run",
+        "Knock Off"
+      ]
+    }
+  },
+  "Blaziken": {
+    "Mega Sweeper": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Speed Boost",
+      "item": "Blazikenite",
+      "moves": [
+        "Flare Blitz",
+        "Superpower",
+        "Stone Edge",
+        "Knock Off"
+      ]
+    },
+    "Mixed Attacker": {
+      "level": 50,
+      "evs": {
+        "at": "52",
+        "sa": "244",
+        "sp": "212"
+      },
+      "ivs": {
+        "hp": "30",
+        "def": "30"
+      },
+      "nature": "Naive",
+      "ability": "Speed Boost",
+      "item": "Life Orb",
+      "moves": [
+        "Overheat",
+        "Hidden Power Ice",
+        "Superpower",
+        "High Jump Kick"
+      ]
+    },
+    "Baton Pass": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "df": "4",
+        "sp": "252"
+      },
+      "nature": "Impish",
+      "ability": "Speed Boost",
+      "item": "Focus Sash",
+      "moves": [
+        "Swords Dance",
+        "Protect",
+        "Baton Pass",
+        "Blaze Kick"
+      ]
+    }
+  },
+  "Breloom": {
+    "Offensive Spore": {
+      "level": 50,
+      "evs": {
+        "hp": "4",
+        "at": "252",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Technician",
+      "item": "Focus Sash",
+      "moves": [
+        "Spore",
+        "Mach Punch",
+        "Bullet Seed",
+        "Rock Tomb"
+      ]
+    }
+  },
+  "Chansey": {
+    "Standard": {
+      "level": 50,
+      "evs": {
+        "hp": "44",
+        "df": "225",
+        "sd": "212"
+      },
+      "nature": "Bold",
+      "ability": "Natural Cure",
+      "item": "Eviolite",
+      "moves": [
+        "Seismic Toss",
+        "...",
+        "...",
+        "..."
+      ]
+    },
+    "Minimize": {
+      "level": 50,
+      "evs": {
+        "df": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "nature": "Timid",
+      "ability": "Natural Cure",
+      "item": "Eviolite",
+      "moves": [
+        "Seismic Toss",
+        "...",
+        "...",
+        "..."
+      ]
+    }
+  },
+  "Conkeldurr": {
+    "Assault Vest": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "at": "252",
+        "df": "4"
+      },
+      "nature": "Adamant",
+      "ability": "Guts",
+      "item": "Assault Vest",
+      "moves": [
+        "Drain Punch",
+        "Mach Punch",
+        "Knock Off",
+        "Ice Punch"
+      ]
+    }
+  },
   "Cresselia": {
     "Standard Defensive Cres": {
       "level": 50,
@@ -15,6 +300,117 @@
         "Psychic",
         "Moonblast",
         "Psyshock"
+      ]
+    }
+  },
+  "Dragonite": {
+    "Dragon Dance": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Multiscale",
+      "item": "Lum Berry",
+      "moves": [
+        "Outrage",
+        "Extreme Speed",
+        "Earthquake",
+        "Fire Punch"
+      ]
+    },
+    "Choice Band": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Multiscale",
+      "item": "Choice Band",
+      "moves": [
+        "Outrage",
+        "Extreme Speed",
+        "Earthquake",
+        "Fire Punch"
+      ]
+    }
+  },
+  "Excadrill": {
+    "Choice Scarf": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "hp": "4",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Mold Breaker",
+      "item": "Choice Scarf",
+      "moves": [
+        "Earthquake",
+        "Iron Head",
+        "Rock Slide",
+        "Horn Drill"
+      ]
+    },
+    "Sand Sweeper": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "df": "4",
+        "sp": "252"
+      },
+      "nature": "Adamant",
+      "ability": "Sand Rush",
+      "item": "Life Orb",
+      "moves": [
+        "Earthquake",
+        "Iron Head",
+        "Rock Slide",
+        "Swords Dance"
+      ]
+    },
+    "Versatile Attacker": {
+      "level": 50,
+      "evs": {
+        "at": "252",
+        "sd": "4",
+        "sp": "252"
+      },
+      "nature": "Jolly",
+      "ability": "Mold Breaker",
+      "item": "Assault Vest",
+      "moves": [
+        "Earthquake",
+        "Iron Head",
+        "Rock Tomb",
+        "Horn Drill"
+      ]
+    }
+  },
+  "Ferrothorn": {
+    "Wall-Nut": {
+      "level": 50,
+      "evs": {
+        "hp": "252",
+        "df": "252",
+        "sd": "4"
+      },
+      "ivs": {
+        "sp": "0"
+      },
+      "nature": "Relaxed",
+      "ability": "Iron Barbs",
+      "item": "Leftovers",
+      "moves": [
+        "Power Whip",
+        "Gyro Ball",
+        "Knock Off",
+        "Iron Head"
       ]
     }
   },
@@ -39,9 +435,14 @@
     "All-Out Attacker": {
       "level": 50,
       "evs": {
-        "sa": "252",
-        "sd": "4",
-        "sp": "252"
+        "sa": "248",
+        "sd": "12",
+        "sp": "248"
+      },
+      "ivs": {
+        "at": "0",
+        "sa": "30",
+        "sp": "30"
       },
       "nature": "Timid",
       "ability": "Contrary",
@@ -50,7 +451,7 @@
         "Leaf Storm",
         "Dragon Pulse",
         "Hidden Power Fire",
-        "Hidden Power Ground"
+        "Giga Drain"
       ]
     }
   }
