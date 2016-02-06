fe7c68d8200ecba1f9e340b500b0afed2b75156a
DragonWhale <alex-a93@hotmail.com>
2016-02-06 18:02:48 +0900

    creating randbats sets

---
+++ script_res/setdex_randbats.js
@@ -0,0 +1,93 @@
+var SETDEX_RANDBATS={
+//some levels and items are still a bit inaccurate. hmu with changes
+  "Venusaur": {
+    "Randbats Regular": {
+      "level": 79,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "ivs": {"at": 30, "sa": 30, "sp": 30},
+      "nature": "Serious",
+      "ability": "Chlorophyll",
+      "item": "Life Orb",
+      "moves": ["Giga Drain", "Sludge Bomb", "Hidden Power Fire", "Sunny Day"]
+    },
+    "Randbats Mega": {
+      "level": 75,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "ivs": {"at": 30, "sa": 30, "sp": 30},
+      "nature": "Serious",
+      "ability": "Chlorophyll",
+      "item": "Venusaurite",
+      "moves": ["Giga Drain", "Sludge Bomb", "Hidden Power Fire", "Earthquake"]
+    }
+  },
+  "Charizard": {
+    "Randbats Regular Physical": {
+      "level": 79,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability": "Blaze",
+      "item": "Flying Gem",
+      "moves": ["Flare Blitz", "Acrobatics", "Earthquake", "Swords Dance"]
+    },
+    "Randbats Regular Special": {
+      "level": 79,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability": "Blaze",
+      "item": "Life Orb" //I dunno what this one has lol
+      "moves": ["Fire Blast", "Air Slash", "Focus Blast", "Roost"]
+    },
+    "Randbats Mega X": {
+      "level": 75,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability": "Blaze",
+      "item": "Charizardite X",
+      "moves": ["Dragon Dance", "Flare Blitz", "Dragon Claw", "Earthquake"]
+    },
+    "Randbats Mega Y": {
+      "level": 75,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability" "Blaze",
+      "item": "Charizardite Y",
+      "moves": ["Fire Blast", "Air Slash", "Solarbeam", "Focus Blast"]
+    }
+  },
+  "Blastoise": {
+    "Randbats Regular": {
+      "level": 79,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability": "Torrent",
+      "item": "Leftovers",
+      "moves": ["Ice Beam", "Dragon Tail", "Rapid Spin", "Scald"]
+    },
+    "Randbats Mega Offensive": {
+      "level": 77,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability": "Torrent",
+      "item": "Blastoiseite",
+      "moves": ["Aura Sphere", "Ice Beam", "Hydro Pump", "Dark Pulse"]
+    },
+    "Randbats Mega Defensive": {
+      "level": 79,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability": "Torrent",
+      "item": "Blastoiseite",
+      "moves": ["Toxic", "Scald", "Dragon Tail", "Rapid Spin"]
+    }
+  },
+  "Butterfree": {
+    "Randbats": {
+      "level": 79,
+      "evs": {"hp": 84, "at": 84, "df": 84, "sa": 84, "sd": 84, "sp": 84},
+      "nature": "Serious",
+      "ability": "Compoundeyes",
+      "item": "Leftovers",
+      "moves": ["Bug Buzz", "Psychic", "Giga Drain", "Quiver Dance"]
+    }
+  }
+}
