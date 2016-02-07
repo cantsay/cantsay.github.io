8933a9875b7fc879befa97d25b6bf08eca0d3263
DragonWhale <alex-a93@hotmail.com>
2016-02-07 11:47:24 +0900

    debugging

--- script_res/setdex_randbats.js
+++ script_res/setdex_randbats.js
@@ -19,7 +19,7 @@ var SETDEX_RANDBATS={
       "item": "Venusaurite",
       "moves": ["Giga Drain", "Sludge Bomb", "Hidden Power Fire", "Earthquake"]
     }
-  }/*,
+  },
   "Charizard": {
     "Randbats Regular Physical": {
       "level": 79,
@@ -53,7 +53,7 @@ var SETDEX_RANDBATS={
       "item": "Charizardite Y",
       "moves": ["Fire Blast", "Air Slash", "Solar Beam", "Focus Blast"]
     }
-  },
+  }/*,
   "Blastoise": {
     "Randbats Regular": {
       "level": 79,
