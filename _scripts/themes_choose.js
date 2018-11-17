bb2709a92e92748e3828af401d447310c01d55a7
LegoFigure11 <legofigurefb@gmail.com>
2018-11-17 22:42:11 +1100

    Travis

--- _scripts/themes_choose.js
+++ _scripts/themes_choose.js
@@ -13,7 +13,7 @@ var changetheme = function () {
 		removejscssfile("_css/legacy_nb_calc.css", "css");
 		removejscssfile("_css/volc_ap_calc.css", "css");
 		removejscssfile("_css/volc_nb_calc.css", "css");
-		if (document.getElementById('midimg') != null) document.getElementById("midimg").src = "_images/aegi trozei.png";
+		if (document.getElementById("midimg") != null) document.getElementById("midimg").src = "_images/aegi trozei.png";
 	} else if (document.getElementById("theme").value === "dark") {
 		loadjscssfile("_css/dark_ap_calc.css", "css");
 		loadjscssfile("_css/dark_nb_calc.css", "css");
@@ -26,7 +26,7 @@ var changetheme = function () {
 		removejscssfile("_css/ap_calc.css", "css");
 		removejscssfile("_css/nb_calc.css", "css");
 		createCookie("selectedItem", selectedItem, 3000);
-		if (document.getElementById('midimg') != null) document.getElementById("midimg").src = "_images/shiny aegi trozei.png";
+		if (document.getElementById("midimg") != null) document.getElementById("midimg").src = "_images/shiny aegi trozei.png";
 		console.log("loaded dark theme");
 	} else if (document.getElementById("theme").value === "legacy") {
 		loadjscssfile("_css/legacy_ap_calc.css", "css");
@@ -38,7 +38,7 @@ var changetheme = function () {
 		removejscssfile("_css/volc_nb_calc.css", "css");
 		removejscssfile("_css/ap_calc.css", "css");
 		removejscssfile("_css/nb_calc.css", "css");
-		if (document.getElementById('midimg') != null) document.getElementById("midimg").src = "_images/cantsay.png";
+		if (document.getElementById("midimg") != null) document.getElementById("midimg").src = "_images/cantsay.png";
 		var select = document.getElementById("theme");
 		var selectedItem = select.value;
 		createCookie("selectedItem", selectedItem, 3000);
@@ -56,7 +56,7 @@ var changetheme = function () {
 		removejscssfile("_css/legacy_nb_calc.css", "css");
 		removejscssfile("_css/ap_calc.css", "css");
 		removejscssfile("_css/nb_calc.css", "css");
-		if (document.getElementById('midimg') != null) document.getElementById("midimg").src = "/_images/pika.jpg";
+		if (document.getElementById("midimg") != null) document.getElementById("midimg").src = "/_images/pika.jpg";
 		var select = document.getElementById("theme");
 		var selectedItem = select.value;
 		createCookie("selectedItem", selectedItem, 3000);
@@ -71,7 +71,7 @@ var changetheme = function () {
 		removejscssfile("_css/legacy_nb_calc.css", "css");
 		removejscssfile("_css/ap_calc.css", "css");
 		removejscssfile("_css/nb_calc.css", "css");
-		if (document.getElementById('midimg') != null) document.getElementById("midimg").src = "_images/volc.png";
+		if (document.getElementById("midimg") != null) document.getElementById("midimg").src = "_images/volc.png";
 		var select = document.getElementById("theme");
 		var selectedItem = select.value;
 		createCookie("selectedItem", selectedItem, 3000);
