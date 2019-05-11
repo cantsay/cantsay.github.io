332496744b5a2201c1e4e548e419c70df2156293
LegoFigure11 <legofigurefb@gmail.com>
2019-05-11 23:39:55 +1000

    Fix rounding error

--- _scripts/ko_chance_comprehensive.js
+++ _scripts/ko_chance_comprehensive.js
@@ -1,4 +1,4 @@
-function getKOChanceText(damage, move, defender, field, isBadDreams, attacker, isMinimized, isVictoryStar) {
+function getKOChanceText(damage, move, defender, field, isBadDreams, attacker, isMinimized, isVictoryStar, critDamage) {
 	if (isNaN(damage[0])) {
 		return "something broke; please tell cant say or LegoFigure11";
 	}
