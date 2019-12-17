1b809790be782f78c9fa8d6ce014facb4b1a9861
LegoFigure11 <legofigurefb@gmail.com>
2019-12-17 22:50:03 +1100

    CB Body Press

--- _scripts/damage.js
+++ _scripts/damage.js
@@ -567,7 +567,7 @@ function getDamageResult(attacker, defender, move, field) {
 		atMods.push(0x2000);
 		description.attackerItem = attacker.item;
 	} else if (attacker.item === "Soul Dew" && (attacker.name === "Latios" || attacker.name === "Latias") && move.category === "Special" ||
-            attacker.item === "Choice Band" && move.category === "Physical" ||
+            attacker.item === "Choice Band" && (move.category === "Physical" || move.name === "Body Press") ||
             attacker.item === "Choice Specs" && move.category === "Special" && !move.isZ) {
 		atMods.push(0x1800);
 		description.attackerItem = attacker.item;
