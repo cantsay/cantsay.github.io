fceb0fc0b7746844149212d26f723c96972555cd
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2020-11-09 23:40:00 +1100

    Default to Gen 8

--- _scripts/ap_calc.js
+++ _scripts/ap_calc.js
@@ -1425,12 +1425,12 @@ $(document).ready(function () {
 			break;
 
 		default:
-			$("#gen7").prop("checked", true);
-			$("#gen7").change();
+			$("#gen8").prop("checked", true);
+			$("#gen8").change();
 		}
 	} else {
-		$("#gen7").prop("checked", true);
-		$("#gen7").change();
+		$("#gen8").prop("checked", true);
+		$("#gen8").change();
 	}
 	for (var n = 1; n < 101; n++) {
 		$("#autolevel-select").append($("<option />").val(n).text(n));
