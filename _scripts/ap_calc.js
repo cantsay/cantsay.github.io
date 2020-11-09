dc4434cea66da7bf2ab940486675af9df112a01e
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2020-11-10 10:07:13 +1100

    Fix move search

--- _scripts/ap_calc.js
+++ _scripts/ap_calc.js
@@ -1465,7 +1465,7 @@ $(document).ready(function () {
 		"dropdownAutoWidth": true,
 		"matcher": function (term, text) {
 			// 2nd condition is for Hidden Power
-			return text.toUpperCase().indexOf(term.toUpperCase()) === 0 || text.toUpperCase().indexOf(" " + term.toUpperCase() !== -1);
+			return text.toUpperCase().indexOf(term.toUpperCase()) === 0 || text.toUpperCase().indexOf(" " + term.toUpperCase()) !== -1;
 		}
 	});
 	$(".set-selector").val(getSetOptions()[gen > 3 ? 1 : gen === 1 ? 5 : 3].id);
